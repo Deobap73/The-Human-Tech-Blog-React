602166548f74ab350e9bf6a1b002cb93b1aa3ef8
Deobap73 <contact@deolindobaptista.com>
2025-04-27 20:02:06 +0200

    Small change of styles
     Changes to be committed:
     	modified:   src/App.tsx
     	modified:   src/global.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // The-Human-Tech-Blog-React/src/App.tsx
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from './routes/PrivateRoute';
-import LoginPage from './pages/login/LoginPage';
-import AdminPage from './pages/AdminPage/AdminPage';
 import Navbar from './components/navbar/Navbar';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/login/LoginPage';
+import AdminPage from './pages/AdminPage/AdminPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 
 function App() {
