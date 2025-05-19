20589458be49a812e5ba94d40ca848bf7141921e
Deobap73 <contact@deolindobaptista.com>
2025-05-19 18:47:19 +0200

    Fix: Update Node engine requirement to support lint-staged@16
    - Updated "engines" field in package.json to require Node >=20.18.0
    - Ensured compatibility with lint-staged@16.0.0 which requires Node >=20.18
    - Synced .nvmrc to reflect the new Node version
    - Cleaned and reinstalled dependencies with NPM
    
     Changes to be committed:
    	modified:   .eslintrc.cjs
    	new file:   .npmrc
    	new file:   .nvmrc
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    pnpm-lock.yaml
    	deleted:    pnpm-workspace.yaml
    	modified:   tsconfig.json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,28 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-    es2024: true,
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname,
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true },
-  },
-  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-refresh', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-  ],
-  settings: { react: { version: 'detect' } },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'prettier/prettier': ['error'],
-  },
-};
+)
