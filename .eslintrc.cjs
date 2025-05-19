7bb6431f835ae0460c33dcbe3570b05bca6d00db
Deobap73 <contact@deolindobaptista.com>
2025-05-19 14:16:43 +0200

    chore(devops): approve build scripts and drop prepare hook
     Changes to be committed:
    	new file:   .eslintrc.cjs
    	deleted:    eslint.config.js
    	modified:   package.json

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2024: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-refresh', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error'],
+  },
+};
